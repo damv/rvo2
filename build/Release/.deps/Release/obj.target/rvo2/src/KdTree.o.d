3b83e66fed960fd856591d749f635f39e5e89a03
Damien Vielpeau <dvi@outsight.tech>
2020-02-04 18:36:21 +0100

    Update with build on my computer...

---
+++ build/Release/.deps/Release/obj.target/rvo2/src/KdTree.o.d
@@ -0,0 +1,11 @@
+cmd_Release/obj.target/rvo2/src/KdTree.o := g++ '-DNODE_GYP_MODULE_NAME=rvo2' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/dvi/.cache/node-gyp/8.10.0/include/node -I/home/dvi/.cache/node-gyp/8.10.0/src -I/home/dvi/.cache/node-gyp/8.10.0/deps/openssl/config -I/home/dvi/.cache/node-gyp/8.10.0/deps/openssl/openssl/include -I/home/dvi/.cache/node-gyp/8.10.0/deps/uv/include -I/home/dvi/.cache/node-gyp/8.10.0/deps/zlib -I/home/dvi/.cache/node-gyp/8.10.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -Wall -O3 -fopenmp -O3 -fno-omit-frame-pointer -fno-rtti -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/rvo2/src/KdTree.o.d.raw   -c -o Release/obj.target/rvo2/src/KdTree.o ../src/KdTree.cpp
+Release/obj.target/rvo2/src/KdTree.o: ../src/KdTree.cpp ../src/KdTree.h \
+ ../src/Definitions.h ../src/Vector2.h ../src/Agent.h \
+ ../src/RVOSimulator.h ../src/Obstacle.h
+../src/KdTree.cpp:
+../src/KdTree.h:
+../src/Definitions.h:
+../src/Vector2.h:
+../src/Agent.h:
+../src/RVOSimulator.h:
+../src/Obstacle.h:
---
+++ build/Release/.deps/Release/rvo2.node.d
@@ -0,0 +1,1 @@
+cmd_Release/rvo2.node := rm -rf "Release/rvo2.node" && cp -af "Release/obj.target/rvo2.node" "Release/rvo2.node"
